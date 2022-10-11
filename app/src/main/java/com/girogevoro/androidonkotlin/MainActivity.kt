fe5cd18469bb06a83b1a030a650a4af8b4d29089
egorovin <egorovin@mail.ru>
2022-10-12 00:10:13 +0300

    Перевести MainActivity на Kotlin. | Сконфигурировать Kotlin

---
+++ app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.girogevoro.androidonkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.girogevoro.androidonkotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
