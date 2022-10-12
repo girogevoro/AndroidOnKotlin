c51cc0edfcbad0a0cff535d7ae0808666d346fd5
egorovin <egorovin@mail.ru>
2022-10-12 04:30:45 +0300

    Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран.

--- app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.View
 import com.girogevoro.androidonkotlin.databinding.ActivityMainBinding
 
+private val TAG = "mytag"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,15 +15,41 @@ class MainActivity : AppCompatActivity() {
         val data = DataView("world")
         setContentView(binding.root)
 
-        binding.button.setOnClickListener(object :View.OnClickListener{
+        binding.button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Log.d("mytag","click")
-                binding.textView1.text = data.name +" "+ data.type
+                Log.d(TAG, "click")
+                binding.textView1.text = "${data.name}  ${data.type}"
             }
         })
 
+        Log.d(TAG, Repository.text)
 
+        val value = object : Base(3, 5){}
+        val copy = value.copy()
+        Log.d(TAG,copy.toString())
+
+
+        //Log.d(TAG, )
     }
 }
 
-data class DataView(val name:String, val type:String = "Hello")
+ data class DataView(val name: String, val type: String = "Hello")
+
+object Repository {
+    val text = "new"
+}
+
+open class Base(var a: Int = 1, var b: Int = 2) {
+    fun result(): Int {
+        return a + b
+    }
+
+    fun copy(): Base {
+        return Base(a, b)
+    }
+
+    override fun toString(): String {
+        return "Base(a=$a, b=$b)"
+    }
+
+}
