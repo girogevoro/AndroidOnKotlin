7d3b6f53fe83866cbc86f1f3dbce771c52797517
egorovin <egorovin@mail.ru>
2022-10-12 03:08:58 +0300

    Добавить кнопку в разметку и повесить на неё clickListener в Activity.

--- app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
@@ -2,11 +2,16 @@ package com.girogevoro.androidonkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.girogevoro.androidonkotlin.R
+import android.view.View
+import com.girogevoro.androidonkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
     }
 }
