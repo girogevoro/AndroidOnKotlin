c1e201aa2302c96f22db69340f2856f8b14be49d
egorovin <egorovin@mail.ru>
2022-10-12 04:06:24 +0300

    Сформировать data class с двумя свойствами и вывести их на экран приложения

--- app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
+++ app/src/main/java/com/girogevoro/androidonkotlin/MainActivity.kt
@@ -10,15 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
-
+        val data = DataView("world")
         setContentView(binding.root)
 
         binding.button.setOnClickListener(object :View.OnClickListener{
             override fun onClick(v: View?) {
                 Log.d("mytag","click")
+                binding.textView1.text = data.name +" "+ data.type
             }
         })
 
 
     }
 }
+
+data class DataView(val name:String, val type:String = "Hello")
